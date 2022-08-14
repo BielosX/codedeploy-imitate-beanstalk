a1fbd99f6c7e35a6455d84dd6561f80b36f38e6b
Mateusz Biel <matbiel@gmail.com>
2022-08-14 17:06:15 +0200

    Remove server.sh

--- scripts/server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-PID_FILE=/home/app/app.pid
-cd /home/app || exit
-source env/bin/activate
-gunicorn --bind :5000 --workers 3 --threads 2 app:app --log-level debug & echo $! > $PID_FILE
