cbd2cc91cc69b2faaf2c5cc96c4fc8a95e810283
Mateusz Biel <matbiel@gmail.com>
2022-08-21 18:00:52 +0200

    Update

--- infra/code_deploy/main.tf
+++ infra/code_deploy/main.tf
@@ -121,22 +121,22 @@ resource "aws_iam_role" "code-deploy-service-role" {
   ]
 }
 
-resource "aws_iam_instance_profile" "demo-app-instance-profile" {
+resource "aws_iam_instance_profile" "app-instance-profile" {
   role = var.role-id
 }
 
-resource "aws_launch_template" "demo-app-launch-template" {
+resource "aws_launch_template" "app-launch-template" {
   name = "${var.app-name}-launch-template"
   instance_type = "t3.micro"
   image_id = data.aws_ami.app-image.id
   vpc_security_group_ids = [aws_security_group.instance-security-group.id]
   user_data = base64encode(join("\n", [for key, value in var.environment_variables : "${key}=${value}"]))
   iam_instance_profile {
-    arn = aws_iam_instance_profile.demo-app-instance-profile.arn
+    arn = aws_iam_instance_profile.app-instance-profile.arn
   }
 }
 
-resource "aws_autoscaling_group" "demo-app-group" {
+resource "aws_autoscaling_group" "app-group" {
   max_size = var.max-instances
   min_size = var.min-instances
   health_check_type = "ELB"
@@ -145,20 +145,19 @@ resource "aws_autoscaling_group" "demo-app-group" {
     "OldestLaunchTemplate"
   ]
   launch_template {
-    id = aws_launch_template.demo-app-launch-template.id
-    version = aws_launch_template.demo-app-launch-template.latest_version
+    id = aws_launch_template.app-launch-template.id
+    version = aws_launch_template.app-launch-template.latest_version
   }
-  target_group_arns = [aws_lb_target_group.app-target-group.arn]
   vpc_zone_identifier = var.app-subnets
   instance_refresh {
     strategy = "Rolling"
     preferences {
-      min_healthy_percentage = 100
+      min_healthy_percentage = 90
     }
   }
 }
 
-resource "aws_codedeploy_app" "demo-app" {
+resource "aws_codedeploy_app" "app" {
   compute_platform = "Server"
   name = var.app-name
 }
@@ -171,11 +170,11 @@ resource "aws_codedeploy_deployment_config" "app-deployment-config" {
   }
 }
 
-resource "aws_codedeploy_deployment_group" "demo-app-deployment-group" {
-  app_name = aws_codedeploy_app.demo-app.name
+resource "aws_codedeploy_deployment_group" "app-deployment-group" {
+  app_name = aws_codedeploy_app.app.name
   deployment_group_name = "${var.app-name}-deployment-group"
   service_role_arn = aws_iam_role.code-deploy-service-role.arn
-  autoscaling_groups = [aws_autoscaling_group.demo-app-group.id]
+  autoscaling_groups = [aws_autoscaling_group.app-group.id]
   deployment_config_name = aws_codedeploy_deployment_config.app-deployment-config.id
   load_balancer_info {
     target_group_info {
